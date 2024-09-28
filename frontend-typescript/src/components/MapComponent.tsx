aec4406bd39c72f26d32efd3cc90d6d66d50748f
tofumiyoshi <tofumiyoshi@gmail.com>
2024-09-28 11:26:25 +0900

    Refactor MapComponent to remove unused imports and add video overlay click functionality

--- frontend-typescript/src/components/MapComponent.tsx
+++ frontend-typescript/src/components/MapComponent.tsx
@@ -1,18 +1,72 @@
-import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap, VideoOverlay } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+//import VideoOverlay from './VideoOverlay';
+import VideoOverlay from './VideoOverlay';
 
+import 'leaflet/dist/leaflet.css';
 
 // Map: 地図を表示するコンポーネント
 // center: [number, number]: 地図の中心座標
 // zoom: number: 地図のズームレベル
+// MapContainer Style: 地図のスタイル
+//  height: string: 地図の高さ
+//  width: string: 地図の幅
+// interactiveOptions:  
+//    zoomControl: boolean: ズームコントロールの表示フラグ
+//    zoomSnap: number: ズームスナップ
+//    zoomDelta: number: ズームデルタ
+//    doubleClickZoom: boolean: ダブルクリックズームの表示フラグ
+//    boxZoom: boolean: ボックスズームの表示フラグ
+//    touchZoom: boolean: タッチズームの表示フラグ
+//    scrollWheelZoom: boolean: スクロールホイールズームの表示フラグ
+//    closePopupOnClick: boolean: ポップアップクリックの表示フラグ
+//    dragging: boolean: ドラッグの表示フラグ
+//    trackResize: boolean: リサイズのトラッキングフラグ
+//    tap: boolean: タップの表示フラグ
+// printable: boolean: プリントの表示フラグ
+// downloadable: boolean: ダウンロードの表示フラグ
 
 // TileLayer: 地図のタイルを表示するコンポーネント
 // osm: OpenStreetMapのタイルを表示するコンポーネント
 //   url: string: タイルのURL
 //   attribution: string: タイルの著作権表示
+// Marker: マーカーを表示するコンポーネント
+//   key: string: マーカーのキー
+//   position: [number, number]: マーカーの座標
+//   markicon: マーカーアイコンを表示するコンポーネント
+//     iconUrl: string: アイコンのURL
+//     iconSize: [number, number]: アイコンのサイズ
+//     iconAnchor: [number, number]: アイコンのアンカー
+//     popupAnchor: [number, number]: ポップアップのアンカー
+//   Popup: ポップアップを表示するコンポーネント
+//     html: string: ポップアップのHTML 
 
-mapRef = useRef<HTMLVideoElement>(null);
+// ImageOverlay: 画像オーバーレイを表示するコンポーネント
+//   url: string: 画像のURL
+//   bounds: [[number, number], [number, number]]: 画像の境界
+//   opacity: number: 画像の透明度
+//   interactive: boolean: インタラクティブのフラグ
+// VideoOverlay: ビデオオーバーレイを表示するコンポーネント
+//   url: string: ビデオのURL
+//   bounds: [[number, number], [number, number]]: ビデオの境界
+//   autoplay: boolean: 自動再生のフラグ
+//   loop: boolean: ループのフラグ
+//   keepAspectRatio: boolean: アスペクト比を保持するフラグ
+//   muted: boolean: ミュートのフラグ
+//   playsinline: boolean: インライン再生のフラグ
+
+// user: User: ユーザー情報
+//   email: string: ユーザーのメールアドレス
+//   name: string: ユーザー名
+//   type: ContentType: ユーザーのコンテンツタイプ
+//   url: string: ユーザーのURL
+//   showLocation(): void: ユーザーの位置情報を表示する関数
+//      isanimate: boolean: アニメーションのフラグ
+//   marker: Marker: ユーザーのマーカー
+//      geo-location-flag: boolean: ユーザーの位置情報のフラグ
+//      current-location: Location: ユーザーの現在位置情報
+//        latitude: number: 緯度
+//        longitude: number: 経度
 
 
 interface MapComponentProps {
@@ -36,32 +90,15 @@ const MapComponent: React.FC<MapComponentProps> = ({ latitude, longitude, zoom, 
     [latitude - 0.02, longitude - 0.02],
     [latitude + 0.02, longitude + 0.02]
   ];
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  const handleVideoClick = () => {
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  };
-
-  useEffect(() => {
-    handleVideoClick();
-  }, []);
 
   return (
-    <div style={{ height: "70vh", width: "100vw" }}>
-      <MapContainer style={{ height: "70vh", width: "100vw" }}>
-        <ChangeView center={center} zoom={13} />
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[latitude, longitude]}>
-          <Popup>
-            {info}
-          </Popup>
-        </Marker>
-        <VideoOverlay url={videoUrl} bounds={bounds} ref={videoRef} eventHandlers={{ click: handleVideoClick }}/>
-      </MapContainer>
-    </div>
+    <MapContainer style={{ height: "70vh", width: "100vw" }}>
+      <ChangeView center={center} zoom={13} />
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+      />
+      <VideoOverlay url={videoUrl} bounds={bounds} play={false} muted={true} interactive={true}/>
+    </MapContainer>
   );
 };
 
